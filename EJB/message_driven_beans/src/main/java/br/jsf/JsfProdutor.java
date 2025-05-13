5b75cac54edbbe3657c959425b32a12b2c77de7b
Giovani L. Rozza <giovanni.rozza@gmail.com>
2025-05-13 20:41:03 -0300

    moved jsf package to br.jsf

--- EJB/message_driven_beans/src/main/java/br/jsf/JsfProdutor.java
+++ EJB/message_driven_beans/src/main/java/br/jsf/JsfProdutor.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
  */
-package jsf;
+package br.jsf;
 
 import javax.annotation.Resource;
 import javax.inject.Named;
